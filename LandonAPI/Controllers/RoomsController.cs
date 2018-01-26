3dcb39b9e9b598fa4845b798ef8451415339fdd7
Anthonie Lance <alance33@gmail.com>
2018-01-26 14:45:42 -0500

    Module 02 - Building a Basic API

---
+++ LandonAPI/Controllers/RoomsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LandonAPI.Controllers
+{
+    [Route("/[controller]")]
+    public class RoomsController : Controller
+    {
+        [HttpGet(Name = nameof(GetRooms))]
+        public IActionResult GetRooms()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ LandonAPI/Models/ApiError.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+using System.ComponentModel;
+
+namespace LandonAPI.Models
+{
+    public class ApiError
+    {
+        public string Message { get; set; }
+        public string Detail { get; set; }
+
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore, DefaultValueHandling = DefaultValueHandling.Ignore)]
+        [DefaultValue("")]
+        public string StackTrace { get; set; }
+    }
+}
